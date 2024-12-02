147e86083874e6180d1bd39dc12b1cb021d72a68
Rajeevalochan R <rajeeevalochan@gmail.com>
2024-12-02 18:39:00 +0530

    Image eslint fix

---
+++ src/app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return(
+        <main className="p-5">
+            <h1 className="text-xl font-bold">About</h1>
+            <p>This is a simple chat application that uses the OpenAI GPT-3 model to generate responses to messages.</p>
+        </main>
+    );
+}
